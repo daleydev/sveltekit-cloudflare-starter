ee2b6f7a5472cdbdc1663ff21c2b13b308e767e5
Daley Chen <mail@daleychen.com>
2026-02-13 20:09:12 -0500

    init

---
+++ src/app.d.ts
@@ -0,0 +1,24 @@
+import type { User, Session } from 'better-auth';
+
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace App {
+		interface Platform {
+			env: Env;
+			ctx: ExecutionContext;
+			caches: CacheStorage;
+			cf?: IncomingRequestCfProperties
+		}
+
+		interface Locals { user?: User; session?: Session }
+
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
+}
+
+export {};
