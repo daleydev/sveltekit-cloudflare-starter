41d2f09b929ad143083b4385493516e6723df387
Daley Chen <mail@daleychen.com>
2026-02-13 23:29:05 -0500

    add more configs

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -3,12 +3,21 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { env } from '$env/dynamic/private';
 import { getRequestEvent } from '$app/server';
-import { db } from '$lib/server/db';
+import type { DrizzleD1Database } from 'drizzle-orm/d1';
 
-export const auth = betterAuth({
-	baseURL: env.ORIGIN,
-	secret: env.BETTER_AUTH_SECRET,
-	database: drizzleAdapter(db, { provider: 'sqlite' }),
-	emailAndPassword: { enabled: true },
-	plugins: [sveltekitCookies(getRequestEvent)] // make sure this is the last plugin in the array
-});
+export const configureAuth = (db: DrizzleD1Database) => {
+	return betterAuth({
+		baseURL: env.ORIGIN,
+		secret: env.BETTER_AUTH_SECRET,
+		database: drizzleAdapter(db, {
+			provider: 'sqlite',
+			usePlural: true
+		}),
+		emailAndPassword: { enabled: true, autoSignIn: true, requireEmailVerification: false },
+		session: {
+			expiresIn: 60 * 60 * 24 * 7, // 7 days
+			updateAge: 60 * 60 * 24 // 1 day
+		},
+		plugins: [sveltekitCookies(getRequestEvent)] // make sure this is the last plugin in the array
+	});
+};
